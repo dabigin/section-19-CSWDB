9d9cd86a0fa12aeaff60e6d8baafc7e19a36afb3
Brandon Dalton <dabigin@gmail.com>
2022-11-07 14:28:21 -0600

    Section 19 Lesson 197 The Break Keyword

--- while_loops/app.js
+++ while_loops/app.js
@@ -14,10 +14,23 @@
 
 // Password Prompt While loop
 //
-const SECRET = "BabyHippo";
+// const SECRET = "BabyHippo";
 
-let guess = prompt("enter the seret code...");
-while (guess !== SECRET) {
-  guess = prompt("enter the secret code...");
+// let guess = prompt("enter the seret code...");
+// while (guess !== SECRET) {
+//   guess = prompt("enter the secret code...");
+// }
+// console.log("CONGRATS YOU GOT THE SECRET!!!");
+
+// Example of how BREAK works
+// let input = prompt("Hey, say something!");
+// while (true) {
+//   input = prompt(input);
+//   if (input.toLowerCase() === "stop copying me") break;
+// }
+// console.log("OK YOU WIN!");
+
+for (let i = 0; i < 1000; i++) {
+  console.log(i);
+  if (i === 100) break;
 }
-console.log("CONGRATS YOU GOT THE SECRET!!!");
