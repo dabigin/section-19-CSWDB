4b35485b1e5b1b0e434563a276fd1e87b894a65a
Brandon Dalton <dabigin@gmail.com>
2022-11-07 13:51:54 -0600

    Add Section 19 Lesson 195 Nested Loops

--- for_loops/app.js
+++ for_loops/app.js
@@ -36,3 +36,26 @@
 // for (let i = 20; i >= 0; i++) {
 //   console.log(i);
 // }
+
+// Nested Loops
+//
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`i is ${i}`);
+//   for (let j = 1; j < 4; j++) {
+//     console.log(`    j is: ${j}`);
+//   }
+// }
+
+const seatingChart = [
+  ["Kristen", "Erik", "Namita"],
+  ["Geoffery", "Juanita", "Antonio", "Kevin"],
+  ["Yuma", "Sakura", "Jack", "Erika"],
+];
+
+for (let i = 0; i < seatingChart.length; i++) {
+  const row = seatingChart[i];
+  console.log(`Row #${i + 1}`);
+  for (let j = 0; j < row.length; j++) {
+    console.log(row[j]);
+  }
+}
