b3ff5b95333aef66f39918dcde5eaaf47bdae1f0
Brandon Dalton <dabigin@gmail.com>
2022-11-08 14:05:42 -0600

    Section 19 Lesson 199 The lovely For..Of Loop

---
+++ for_of/app.js
@@ -0,0 +1,46 @@
+// const subreddits = [
+//   "cringe",
+//   "books",
+//   "chickens",
+//   "funny",
+//   "pics",
+//   "soccer",
+//   "gunners",
+// ];
+
+// //   For Loop of subreddits
+// //
+// for (let i = 0; i < subreddits.length; i++) {
+//   console.log(`Visit reddit.com/r/${subreddits[i]}`);
+// }
+
+// // For..Of loop of subreddits
+// for (let sub of subreddits) {
+//   console.log(`Visit reddit.com/r/${sub}`);
+// }
+
+// // Seating chart constant variable
+// const seatingChart = [
+//   ["Kristen", "Erik", "Namita"],
+//   ["Geoffrey", "Juanita", "Antonio", "Kevin"],
+//   ["Yuma", "Sakura", "Jack", "Erika"],
+// ];
+
+// //For Loop for Seating chart
+// for (let i = 0; i < seatingChart.length; i++) {
+//   const row = seatingChart[i];
+//   for (let j = 0; j < row.length; j++) {
+//     console.log(row[j]);
+//   }
+// }
+
+// //For..Of Loop for Seating chart
+// for (let row of seatingChart) {
+//   for (let student of row) {
+//     console.log(student);
+//   }
+// }
+
+for (let char of "hello world") {
+  console.log(char);
+}
